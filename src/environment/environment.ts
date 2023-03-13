17ccd1b794e29134996a246818f794da2dc932a6
Hamza El Meknassi <hamza.elmeknassi@deecision.com>
2023-03-13 21:49:39 +0100

    Add environment.ts

---
+++ src/environment/environment.ts
@@ -0,0 +1,5 @@
+// You need to create environment.prod.ts based on environment.ts to deploy to production
+export const environment = {
+  production: false,
+  mongoConnectionString: 'mongodb://user:password@localhost:27017',
+};
